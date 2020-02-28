7427d61a47bc4e6b8bcd1ee60935bb30c50e1501
CodeColorist <codecolorist@gmail.com>
2020-02-28 17:54:22 +0800

    Initial Commit

---
+++ XPoCed/AppDelegate.m
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.m
+//  XPoCed
+//
+//  Created by CodeColorist on 2020/2/28.
+//  Copyright © 2020 me.chichou. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
---
+++ XPoCed/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XPoCed
+//
+//  Created by CodeColorist on 2020/2/28.
+//  Copyright © 2020 me.chichou. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
