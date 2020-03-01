207179d223a9c1301122b150a5ba2b36579185cd
CodeColorist <codecolorist@gmail.com>
2020-03-01 16:59:39 +0800

    better share menu

---
+++ App/Controllers/URLSchemesController.h
@@ -0,0 +1,17 @@
+//
+//  URLSchemesController.h
+//  TuttiFrutti
+//
+//  Created by CodeColorist on 2020/3/1.
+//  Copyright © 2020 me.chichou. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface URLSchemesController : NSViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ App/Controllers/URLSchemesController.m
@@ -0,0 +1,22 @@
+//
+//  URLSchemesController.m
+//  TuttiFrutti
+//
+//  Created by CodeColorist on 2020/3/1.
+//  Copyright © 2020 me.chichou. All rights reserved.
+//
+
+#import "URLSchemesController.h"
+
+@interface URLSchemesController ()
+
+@end
+
+@implementation URLSchemesController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+@end
